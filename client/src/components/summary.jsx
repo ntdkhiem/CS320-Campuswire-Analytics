fc322e8403f13c666c98b650fa7b322f2a0b0331
Billy Singh <anshulsingh1312@gmail.com>
2023-11-11 18:42:38 -0500

    Added stats and warnings

--- client/src/components/summary.jsx
+++ client/src/components/summary.jsx
@@ -3,20 +3,43 @@ import './summary.css'
 
 const Summary = () => {
 
+    const warnings = ['420 Unread posts', '96 Unanswered questions', '40 Unanswered followups'];
+    const class_sum = ['191 total posts','575 total contributions','153 instructors\' responses','23 students\' responses','9 min avg. response time'];
     return(
-        <div  className={`container`}>
-            <div className={`left-content`}>
-                <p className={`title`}>
-                    CS 320: Intro to Software Engineering
-                </p>
+        <div>
+            <div  className={`container`}>
+                <div className={`content`}>
+                    <p className={`title`}>
+                        CS 320: Intro to Software Engineering
+                    </p>
+                </div>
+                <div className={`content`}>
+                    <p className={`instructors`}>
+                        Instructor: Matthew Rattigan (you) <br/>
+                        TA: John Doe<br/>
+                        UCA: Jane Doe
+                    </p>
+                </div>         
             </div>
-            <div className={`right-content`}>
-                <p className={`instructors`}>
-                    Instructor: Matthew Rattigan (you) <br/>
-                    TA: John Doe<br/>
-                    UCA: Jane Doe
-                </p>
-            </div>         
+            <div className={`container`}>
+                <div className={`content`}>
+                    {warnings.map((item, id) => (
+                        <a className={`warning`} href={`/`} >
+                            ⚠ {item}<br/>
+                        </a>
+                    ))}
+                </div>
+                <div className={`content`}>
+                    <p className={`class-summary`}>
+                    191 total posts<br/>
+                    575 total contributions<br/>
+                    153 instructors' responses<br/>
+                    23 students' responses<br/>
+                    9 min avg. response time
+                    </p>
+                </div>
+
+            </div>
         </div>
     );
 
