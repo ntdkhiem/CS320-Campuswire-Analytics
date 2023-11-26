2947665e9746789e681fce9d5fbc76fa3dadc7b0
Billy Singh <anshulsingh1312@gmail.com>
2023-11-26 17:47:24 -0500

    contributors page

--- client/src/components/contributors.jsx
+++ client/src/components/contributors.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Card from './card.jsx';
+import './contributors.css';
 
-function contributors() {
+function Contributors() {
+  const contributorsData = [
+    { id: 1, username: 'UserCS320', postsCreated: 3, questionsAnswered: 7, endorsedComments: 5, status: 'Active', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 2, username: 'UserCS321', postsCreated: 1, questionsAnswered: 2, endorsedComments: 0, status: 'Active', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 3, username: 'UserCS322', postsCreated: 0, questionsAnswered: 1, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 4, username: 'UserCS323', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 5, username: 'UserCS324', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 6, username: 'UserCS325', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 7, username: 'UserCS326', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 8, username: 'UserCS327', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+    { id: 9, username: 'UserCS328', postsCreated: 0, questionsAnswered: 0, endorsedComments: 0, status: 'Inactive', avatarSrc: 'https://via.placeholder.com/50' },
+  ];
+
   return (
-    <div>
-      content
+    <div className="container">
+      <div className="contributors-wrapper">
+        {contributorsData.map(contributor => (
+          <Card
+            key={contributor.id}
+            avatarSrc={contributor.avatarSrc}
+            username={contributor.username}
+            postsCreated={contributor.postsCreated}
+            questionsAnswered={contributor.questionsAnswered}
+            endorsedComments={contributor.endorsedComments}
+            status={contributor.status}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default contributors
+export default Contributors;
