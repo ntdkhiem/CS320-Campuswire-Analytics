7342f56263c6583c510cb35d7bb2248abf0b3d40
king-billy <65773820+king-billy@users.noreply.github.com>
2023-12-13 23:06:35 -0500

    spruce-up for presentation

--- client/src/components/summary.jsx
+++ client/src/components/summary.jsx
@@ -11,10 +11,13 @@ const avgTime = avgTimeObj.avg;
 
 const resUnansweredQuestions = await fetch('http://localhost:3001/numUnansweredQuestions')
 const unansweredQuestionsObj = await resUnansweredQuestions.json();
-const unansweredQuestions = unansweredQuestionsObj.total 
+const unansweredQuestions = unansweredQuestionsObj.total
+
+const resFirst = await fetch('http://localhost:3001/top3/first')
+const first = await resFirst.json();
+
 
 const Summary = () => {
-  const warnings = [unansweredQuestions + " Unanswered questions"];
   const boxStyle = {
     backgroundColor: "lightblue",
     height: "100px", // Adjust the height as needed
@@ -23,6 +26,8 @@ const Summary = () => {
     position: "relative",
     right: "50px",
   };
+
+  
   return (
     <div>
       <div className={`container`}>
@@ -41,33 +46,37 @@ const Summary = () => {
       </div>
       <div className={`container`}>
         <div className={`content`}>
-          {warnings.map((item, id) => (
-            <a className={`warning`} href={`/`}>
-              ⚠ {item}
-              <br />
-            </a>
-          ))}
-        </div>
-        <div className={`content`}>
           <p className={`class-summary`}>
             {totalPost} total posts
             <br />
-            575 total contributions
-            <br />
-            153 instructors' responses
-            <br />
-            23 students' responses
-            <br />
             {avgTime} hour avg. response time
+            <br />
+            <span className={`warning`}>⚠ {unansweredQuestions} unanswered questions</span>
           </p>
         </div>
       </div>
 
+      <div>
+      <h2 style={{position:'relative', left:'90px', top:'60px'}}>Top Post</h2>
       <div style={boxStyle}>
-        <div className={"flex-container"}>content</div>
+        <div className={"flex-container"}>
+          <p>
+          <h2 style={{position:'absolute',left:'20px',top:'10px', fontSize:'20px'}}>{first.title}</h2>
+          <span style={{position: 'absolute', left:'20px', top:'25px', margin:'10px'}}>{first.body}</span>
+          </p>
+        
+        </div>
       </div>
-      <div style={boxStyle}>
-        <div className={"flex-container"}>content</div>
+      <h2 style={{position:'relative', left:'90px'}}>Top User</h2>
+      <div style={{...boxStyle, bottom:'60px'}}>
+        <div className={"flex-container"}>
+        <p>
+          <h2 style={{position:'absolute',left:'20px',top:'10px', fontSize:'20px'}}>Anonymous</h2>
+          <img className={'avatar'} src={'https://via.placeholder.com/50'} alt='avatar' style={{position:'relative', left:'340px', top:'8px'}}/>
+          <div className={'dot green'} style={{position:'relative', left:'375px', bottom:'3px'}}></div>
+          </p>
+        </div>
+      </div>
       </div>
     </div>
   );
